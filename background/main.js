ab8ad190ccaeb2afc0d79947cce473ebfa8e146b
Adam <8365885+zvakanaka@users.noreply.github.com>
2023-01-14 10:34:32 -0700

    add keyboard toggle

---
+++ background/main.js
@@ -0,0 +1,55 @@
+/* TODO: CODE COPIED FROM popup.js - MAKE CODE DRY */
+if (typeof browser === "undefined") {
+  var browser = chrome;
+}
+async function getDarkMode() {
+  // https://github.com/mdn/webextensions-examples/blob/master/stored-credentials/storage.js
+  const storedObj = await browser.storage.local.get();
+  if (storedObj === null || typeof storedObj === 'undefined') {
+    await setDarkMode(true);
+    return true;
+  }
+  return storedObj.darkMode;
+}
+
+async function setDarkMode(value, cb = () => { }) {
+  await chrome.storage.local.set({ darkMode: value });
+  cb();
+}
+
+function tabDarkModeSet(_tabs, isDarkMode) {
+  // https://developer.chrome.com/extensions/tabs#method-query
+  chrome.tabs.query({}, (queryTabs) => {
+    queryTabs.forEach(tab => {
+      if (tab.url.startsWith('about:')) {
+        return;
+      }
+      chrome.tabs.sendMessage(tab.id, {
+        command: 'set-dark-mode',
+        isDarkMode
+      });
+    });
+  })
+}
+
+function reportError(err) {
+  console.error(`Failed to set dark mode on tab: ${err}`);
+}
+
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_second_WebExtension
+function messageDarkMode(isDarkMode) {
+  browser.tabs.query({ active: true, currentWindow: true })
+    .then(tabs => tabDarkModeSet(tabs, isDarkMode))
+    .catch(reportError);
+}
+/* END COPIED CODE */
+
+// https://developer.chrome.com/docs/extensions/reference/commands/
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'keyboard-toggle-dark-mode') {
+    getDarkMode().then((isDark) => {
+      messageDarkMode(!isDark);
+      setDarkMode(!isDark);
+    })
+  }
+});
